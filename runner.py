c8a031aa45d1e3fe581c6d2880ce45c1af0fa70b
dgea005 <danielgeals@gmail.com>
2017-01-29 13:45:13 +0100

    now follows new individual listing URLs only, clean up naming and imports

--- runner.py
+++ runner.py
@@ -2,30 +2,29 @@ import logging
 import os
 import time
 import log
-from sqlalchemy import create_engine
+import sqlalchemy
 from apscheduler.schedulers.blocking import BlockingScheduler
 import pandas as pd
 import scrapers
 
 
-
 def scrape_search_index():
     """run full page scraper on first page of search results"""
-    disk_engine = create_engine('sqlite:///database/listings.db')
+    db_connection = sqlalchemy.create_engine('sqlite:///database/listings.db')
     urls = ['http://www.wg-gesucht.de/en/wohnungen-in-Berlin.8.1.0.0.html',
             'http://www.wg-gesucht.de/en/wohnungen-in-Berlin.8.2.0.0.html']
     for url in urls:
         listing_html = scrapers.indexScraper(url).get_html()
         listings = listing_html.get_listings_from_page()
         n_records = listings.shape[0]
-        listings.to_sql('listings_stg', disk_engine, if_exists='append', index=False)
+        listings.to_sql('listings_stg', db_connection, if_exists='append', index=False)
         logging.info('{} records written to listings_stg in listings.db'.format(n_records))
 
 def write_clean_listings():
     """deuplicate data in listings_stg and write to clean version"""
     # get the raw data -- all data scraped to date
-    disk_engine = create_engine('sqlite:///database/listings.db')
-    raw_listings = pd.read_sql('select * from listings_stg', disk_engine)
+    db_connection = sqlalchemy.create_engine('sqlite:///database/listings.db')
+    raw_listings = pd.read_sql('select * from listings_stg', db_connection)
     n_listings = raw_listings.listing_id.nunique()
     logging.info('{} unique listing_ids in listings_stg'.format(n_listings))
 
@@ -43,15 +42,15 @@ def write_clean_listings():
 
     # write processed dataframe
     logging.info('{} listings to write to listings_clean'.format(clean_listings.shape[0]))
-    clean_listings.to_sql('listings_clean', disk_engine, if_exists='replace', index=False)
+    clean_listings.to_sql('listings_clean', db_connection, if_exists='replace', index=False)
     logging.info('clean listings written')
 
 
 def follow_listing_urls():
     """go through listing individual urls to get more detail"""
     # retrieve urls from clean table
-    disk_engine = create_engine('sqlite:///database/listings.db')
-    listings = pd.read_sql('select * from listings_clean', disk_engine)
+    db_connection = sqlalchemy.create_engine('sqlite:///database/listings.db')
+    listings = pd.read_sql('select * from listings_clean', db_connection)
     logging.info('pulled {} distinct urls from local db'.format(listings.shape[0]))
 
     listings.last_scrape_time = pd.to_datetime(listings.last_scrape_time)
@@ -61,36 +60,48 @@ def follow_listing_urls():
     listing_urls = listings.link.tolist()
     logging.info('list of {} listing URLs to scrape'.format(len(listing_urls)))
 
-    # should functionalise below and go through in batches
-    # and write the batches of <5 to db -- so data isn't lost if the scraper at item x
+    ## try to get previous listings_dim details --> this may not exist yet
+    ## so need to check if the table is there first
+    def filter_previously_scraped(URLs):
+        """if listings have been scraped before filter them out"""
+        try:
+            listing_dim = pd.read_sql('select * from listing_dim', db_connection)
+            listing_dim_URLs = listing_dim.link.tolist()
+            unscraped_URLs = [url for url in URLs if url not in listing_dim_URLs]
+            logging.info('{} new listings to scrape'.format(len(unscraped_URLs)))
+            return unscraped_URLs
+        except sqlalchemy.exc.OperationalError:
+            logging.info('listing_dim does not exist yet')
+            return URLs
+
+    listing_urls = filter_previously_scraped(listing_urls)  
 
     # get further details
     listing_details = []
-    for url in listing_urls[0:4]:
+    for url in listing_urls:
         scrape_results = scrapers.listingScraper(url).get_listing_html().parse_details()
         listing_details.append(scrape_results)
+        # running this for all listings quickly creates a lot of requests
+        # this triggers an anti-scraper/bot response - hence the sleep
         logging.info('url: {} scraped; sleeping 10 seconds'.format(url))
         time.sleep(10)
 
     listing_details = pd.DataFrame(listing_details)
-    print(listing_details)
-    logging.info('retrieved {} link details'.format(listing_details.shape[0]))
-    listing_details.to_sql('listing_dim', disk_engine, if_exists='append', index=False)
-    logging.info('{} data written to listings_dim'.format(listing_details.shape))
 
-def run_scheduler():
+    logging.info('retrieved {} link details'.format(listing_details.shape[0]))
+    listing_details.to_sql('listing_dim', db_connection, if_exists='append', index=False)
+    logging.info('{} data written to listing_dim'.format(listing_details.shape))
+
+def start_scheduler():
     """
     running scheduled scraping and other tasks
     """
-    #log.setup_logger()
-
     scheduler = BlockingScheduler()
-    scheduler.add_job(scrape_search_index, 'interval', minutes=2)
-    scheduler.add_job(write_clean_listings, 'interval', minutes=2)
-    # --- currently have individual url scraper disabled - there is a request limit --- #
-    # this is working but has issues
-    # fields are missing...
-    scheduler.add_job(follow_listing_urls, 'interval', minutes=1)
+    scheduler.add_job(scrape_search_index, 'interval', minutes=5)
+    scheduler.add_job(write_clean_listings, 'interval', minutes=5)
+    
+    # this is working but has missing field issues
+    scheduler.add_job(follow_listing_urls, 'interval', minutes=10)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
@@ -100,4 +111,4 @@ def run_scheduler():
 
 if __name__ == '__main__':
     log.setup_logger()
-    run_scheduler()
+    start_scheduler()
