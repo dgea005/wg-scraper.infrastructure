35d3f486a36ff4174254644344b196abc9a03fde
dgea005 <danielgeals@gmail.com>
2017-04-12 09:04:40 +0200

    updated readme and filters

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wg_scraper
+
++ uses mailgun to email latest results
++ data stored in sqlite3 
+TODOS:
++ json input for filters?
--- user_filters.py
+++ user_filters.py
@@ -3,8 +3,8 @@ from pandas import Timestamp
 
 def get_preferences():
     preferences = dict()
-    preferences['min_free_from'] = Timestamp('2017-03-25')
-    preferences['max_free_from'] = Timestamp('2017-05-01')
+    preferences['min_free_from'] = Timestamp('2017-04-25')
+    preferences['max_free_from'] = Timestamp('2017-05-03')
     preferences['max_cost'] = 1000
     preferences['min_cost'] = 300
     preferences['min_length'] = 30
